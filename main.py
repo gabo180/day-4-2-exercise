4fa2e39dc46d291f52bbbac49b2f98c9e582d4ff
Gabriel Hernandez <gabrieldejesushernandez@gmail.com>
2021-11-17 15:02:22 +0000

    Moved "import random" at the top of the program.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import random
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-import random
+
 
 select_name = random.randint(0, (len(names) - 1))
 name_selected = names[select_name]
